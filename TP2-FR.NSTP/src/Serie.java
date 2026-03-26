217817ca018ec82f6cac09b3ebb870f71597bfaa
Nate33-create <2562417@cegeplimoilou.ca>
2026-03-26 13:07:59 -0400

    Updated Serie
    J'ai créer un constructeur pour serie.java

--- TP2-FR.NSTP/src/Serie.java
+++ TP2-FR.NSTP/src/Serie.java
@@ -4,4 +4,9 @@ public class Serie
 {
     private String nom;
     private List<String> listeOuvrages;
+
+    public Serie(String nom, List<String> listeOuvrages) {
+        this.nom = nom;
+        this.listeOuvrages = listeOuvrages;
+    }
 }
